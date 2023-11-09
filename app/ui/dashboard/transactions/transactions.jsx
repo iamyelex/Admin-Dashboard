b397b39a1a98c16d9c47d2f5cb1c0afad785eecf
iamyelex <yelex4love@gmail.com>
2023-11-08 21:37:30 -0800

    transactions section added

--- app/ui/dashboard/transactions/transactions.jsx
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -1,5 +1,86 @@
+import Image from "next/image";
+
 import styles from "@/app/ui/dashboard/transactions/transactions.module.css";
 
 export default function Transactions() {
-  return <div className={styles.container}>Transactions</div>;
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Latest Transactions</h2>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Status</td>
+            <td>Date</td>
+            <td>Amount</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt="user-image"
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.pending} `}>
+                Pending
+              </span>
+            </td>
+            <td>14-02-2023</td>
+            <td>$3.45</td>
+          </tr>
+
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt="user-image"
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.done} `}>Done</span>
+            </td>
+            <td>14-02-2023</td>
+            <td>$3.45</td>
+          </tr>
+
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt="user-image"
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.cancelled} `}>
+                Cancelled
+              </span>
+            </td>
+            <td>14-02-2023</td>
+            <td>$3.45</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
