8aaea5b2a432ecf9f971deb2dafa01ffacee2d50
iamyelex <yelex4love@gmail.com>
2023-11-09 03:27:30 -0800

    login page created

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,5 +1,14 @@
-function LoginPage() {
-  return <div>LoginPage</div>;
-}
+import styles from "@/app/ui/login/login.module.css";
 
-export default LoginPage;
+export default function LoginPage() {
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <h1>Login</h1>
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
+}
